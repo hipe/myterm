f5b4dfb26a07feb86308d110d0fa6066a45421df
Mark Meves <mark.meves@gmail.com>
2011-08-24 13:20:55 -0400

    [#now] these repath changes

--- ._repath.rb
+++ ._repath.rb
@@ -2,47 +2,28 @@
 
 payload = lambda do
 
-  bin_folder="#{`pwd`.strip}/bin"
-  unless File.directory?(bin_folder)
-    return {
-      :message => "not a directory, won't add to PATH: #{bin_folder}"
-    }
-  end
+  dir = File.expand_path('../bin', __FILE__)
+  File.directory?(dir) or return {
+    :message => "not a directory, won't add to PATH: #{dir}"
+  }
 
   path = ENV['PATH']
-  path_parts = path.split(':')
-
-  if path.include?(bin_folder)
-    if true or ARGV.include?('-F')
-      unless path_parts.reject! { |p| p == bin_folder }
-        return {
-          :message => "bin folder \"#{bin_folder}\" not found in path \"#{path}\""
-        }
-      end
-      return {
-        :new_path => ( [bin_folder] + [path_parts] ).join(':'),
-        :message  => "rewriting path to have bin folder at the beginning",
-        :success => true
-      }
-    else
-      if 0 == path.index(bin_folder)
-        return {
-          :message => "bin folder is already at front of PATH",
-          :success => true
-        }
-      else
-        return {
-          :message => "bin folder is in path but not at front.  use -F to rewrite PATH",
-          :status => :not_front,
-          :success => true
-        }
-      end
-    end
-  else
-    path_parts.unshift(bin_folder)
-    return {
-      :new_path => path_parts.join(':'),
+  parts = path.split(':')
+  case parts.index(dir)
+  when nil
+    {
+      :new_path => ([dir] + parts).join(':'),
       :message  => "prepending bin folder to the beginning of the PATH.",
+      :success  => true
+    }
+  when 0
+    { :message  => "already at front of path: \"#{dir}\"",
+      :success  => true
+    }
+  else
+    {
+      :new_path => ([dir] + parts.reject{ |x| x == path }).join(':'),
+      :message  => "rewriting path to have bin folder at the beginning",
       :success  => true
     }
   end
